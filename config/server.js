33c4fe7dde235c18195ea00f622bc212afa84a2b
Pedro Carvalho <ppscarvalho@gmail.com>
2019-09-25 13:19:24 -0300

    Problema no express-validator

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -8,6 +9,7 @@ app.set('views', './app/views')
 
 //Middle
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator.validationResult());
 
 consign().include('app/routes')
 .then('config/dbConnection.js')
