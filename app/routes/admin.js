1af3879f3c5c9c81f54f676982dd42c5ab63070d
Pedro Carvalho <ppscarvalho@gmail.com>
2019-09-25 18:14:25 -0300

    Adiciona express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,16 +1,27 @@
+const {check, validationResult} = require('express-validator/check');
+
 module.exports = function(app) {
-  app.get("/formulario_inclusao_noticia", function(req, res) {
+  app.get("/add_noticia", function(req, res) {
     res.render("admin/form_add_noticia");
   });
 
-  app.post("/noticias/salvar", function(req, res) {
+  app.post("/noticias/salvar", [
+    check('titulo').not().isEmpty().withMessage('Título obrigatório'),
+    check('data_noticia').not().isEmpty().withMessage('Data da Notícia é obrigatória').isISO8601('dd-mm-yyyy')
+
+  ],function(req, res) {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
     var noticia = req.body;
     var conn = app.config.dbConnection();
     var noticiasModel = new app.app.models.NoticiaDAO(conn);
 
     noticiasModel.salvarNoticia(noticia, function(error, result) {
-      res.redirect('/noticias')
+     res.redirect('/noticias')
     });
   });
-
 };
--- config/server.js
+++ config/server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-
+//var expressValidator = require('express-validator');
+//const {check, validationResult} = require('express-validator/check');
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views')
 
 //Middle
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(expressValidator.validationResult());
+//app.use(check());
 
 consign().include('app/routes')
 .then('config/dbConnection.js')
